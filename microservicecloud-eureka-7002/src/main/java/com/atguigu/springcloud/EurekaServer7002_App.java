65d13d66521a10c2914bfa5c82be1f486cf4ffe4
zcl <820861340@qq.com>
2021-10-18 10:58:58 +0800

    initial

--- microservicecloud-eureka-7002/src/main/java/com/atguigu/springcloud/EurekaServer7002_App.java
+++ microservicecloud-eureka-7002/src/main/java/com/atguigu/springcloud/EurekaServer7002_App.java
@@ -4,8 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * EurekaServer服务器端启动类,接受其它微服务注册进来
+ */
 @SpringBootApplication
-@EnableEurekaServer // EurekaServer服务器端启动类,接受其它微服务注册进来
+@EnableEurekaServer
 public class EurekaServer7002_App
 {
 	public static void main(String[] args)
